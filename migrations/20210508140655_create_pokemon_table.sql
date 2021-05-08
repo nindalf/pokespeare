63fe47ddf20adff0d17b0e3160a2d0932c08b15f
Krishna Sundarram <krishna.sundarram@gmail.com>
2021-05-08 15:40:01 +0100

    Add sqlx dependency, migrations/ and scripts/

---
+++ migrations/20210508140655_create_pokemon_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE pokemon (
+    id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    shakespeare_description TEXT
+);
