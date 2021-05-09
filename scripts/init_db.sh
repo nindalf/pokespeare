6fd7cb28225cad34a5d817ef80e6c4bbbc7b2e34
Krishna Sundarram <krishna.sundarram@gmail.com>
2021-05-09 17:13:23 +0100

    Modify actions to make tests work

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -x
 set -eo pipefail
 
@@ -20,9 +20,9 @@ then
 fi
 
 export PGPASSWORD="${DB_PASSWORD}"
-# until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-#   >&2 echo "Postgres is still unavailable - sleeping"
-# sleep 1 done
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+sleep 1 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
